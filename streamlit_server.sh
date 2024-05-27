9566abd49bd2782bc856e39c2b43024ffff6614e
w626394316 <626394316@qq.com>
2024-05-27 23:43:56 +0800

    update all

--- streamlit_server.sh
+++ streamlit_server.sh
@@ -1,1 +1,1 @@
-streamlit run /group_share/www/chat-doraemon/agent/lagent/examples/internlm2_agent_web_demo.py --server.address 127.0.0.1 --server.port 7860
+streamlit run /group_share/www/chat-doraemon/agent/lagent/examples/react_lmdeploy_web_demo.py --server.address 127.0.0.1 --server.port 7860
